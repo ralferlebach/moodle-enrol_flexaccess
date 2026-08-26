021785eb2369163b7488119e8960f5f9dbbc7d25
ralferlebach <ralf.erlebach@gmx.de>
2026-08-26 19:11:57 +0200

    v 0.9.50

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_flexaccess';
-$plugin->version = 2026082426;
+$plugin->version = 2026082427;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502];
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.9.49';
-$plugin->dependencies = ['auth_flexaccess' => 2026082426];
+$plugin->release = '0.9.50';
+$plugin->dependencies = ['auth_flexaccess' => 2026082427];
